3999dd95055fbd657989b65d62f65b2296bd1b8f
julianVelandia <jvelandiag@unal.edu.co>
2023-08-27 18:45:02 -0500

    usecase test

--- internal/student/application/usecase/view_profile_test.go
+++ internal/student/application/usecase/view_profile_test.go
@@ -2,10 +2,8 @@ package usecase_test
 
 import (
 	"errors"
-	"github.com/julianVelandia/EDteam/SOLIDyHexagonal/ProyectoCurso/internal/class/application/command"
 	"testing"
 
-	"github.com/julianVelandia/EDteam/SOLIDyHexagonal/ProyectoCurso/internal/student/application/query"
 	"github.com/julianVelandia/EDteam/SOLIDyHexagonal/ProyectoCurso/internal/student/application/usecase"
 	"github.com/julianVelandia/EDteam/SOLIDyHexagonal/ProyectoCurso/internal/student/domain"
 	"github.com/stretchr/testify/assert"
@@ -15,30 +13,19 @@ const (
 	email      = "test@email.com"
 	classID1   = "class_1"
 	classID2   = "class_2"
-	title      = "title"
 	name       = "julián"
 	className1 = "name 1"
 	className2 = "name 2"
 )
 
 func TestViewExecuteWhenRepositoryResponseOkShouldReturnOK(t *testing.T) {
-
 	classesDomain := []domain.Class{
 		*domain.NewClass(classID1, className1),
 		*domain.NewClass(classID2, className2),
 	}
-
 	profileDomain := *domain.NewProfile(
 		email,
 		name,
-	)
-
-	profileDomain
-	classDomain
-	cmd := *command.NewUpdate(
-		email,
-		classID,
-		title,
 	)
 	repositoryViewProfile := new(RepositoryViewProfileMock)
 
@@ -47,32 +34,45 @@ func TestViewExecuteWhenRepositoryResponseOkShouldReturnOK(t *testing.T) {
 	getUseCase := usecase.NewViewUseCase(
 		repositoryViewProfile,
 	)
-	result, errResult := getUseCase.Execute(qry)
+	resultProfile, resultClasses, errResult := getUseCase.Execute(email)
 
 	assert.NoError(t, errResult)
-	assert.Equal(t, result, classDomain)
+	assert.Equal(t, resultProfile, profileDomain)
+	assert.Equal(t, resultClasses, classesDomain)
 	repositoryViewProfile.AssertExpectations(t)
 }
 
-func TestViewExecuteWhenRepositoryViewFailShouldReturnError(t *testing.T) {
-	qry := *query.NewView(
-		email,
-		classID,
-		title,
-	)
-	repositoryViewMock := new(RepositoryViewClassMock)
+func TestViewExecuteWhenGetProfileByEmailFailShouldReturnError(t *testing.T) {
+	repositoryViewProfile := new(RepositoryViewProfileMock)
 
-	repositoryViewMock.On("GetClassByClassID", classID).Return(
-		domain.Class{},
-		errors.New(""),
-	).Once()
+	repositoryViewProfile.On("GetProfileByEmail", email).Return(domain.Profile{}, errors.New("")).Once()
 	getUseCase := usecase.NewViewUseCase(
-		repositoryViewMock,
-		nil,
+		repositoryViewProfile,
 	)
-	result, errResult := getUseCase.Execute(qry)
+	resultProfile, resultClasses, errResult := getUseCase.Execute(email)
 
 	assert.Error(t, errResult)
-	assert.Equal(t, result, domain.Class{})
-	repositoryViewMock.AssertExpectations(t)
+	assert.Equal(t, resultProfile, domain.Profile{})
+	assert.Equal(t, resultClasses, []domain.Class{})
+	repositoryViewProfile.AssertExpectations(t)
+}
+
+func TestViewExecuteWhenGetClassesDoneByEmailShouldReturnError(t *testing.T) {
+	profileDomain := *domain.NewProfile(
+		email,
+		name,
+	)
+	repositoryViewProfile := new(RepositoryViewProfileMock)
+
+	repositoryViewProfile.On("GetProfileByEmail", email).Return(profileDomain, nil).Once()
+	repositoryViewProfile.On("GetClassesDoneByEmail", email).Return([]domain.Class{}, errors.New("")).Once()
+	getUseCase := usecase.NewViewUseCase(
+		repositoryViewProfile,
+	)
+	resultProfile, resultClasses, errResult := getUseCase.Execute(email)
+
+	assert.Error(t, errResult)
+	assert.Equal(t, resultProfile, domain.Profile{})
+	assert.Equal(t, resultClasses, []domain.Class{})
+	repositoryViewProfile.AssertExpectations(t)
 }
